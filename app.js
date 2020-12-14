0572a1b0ab177c53b6b719e664148f1de044bdde
rithgan <rithikgandhi1989@gmail.com>
2020-12-14 21:23:15 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const sayHello = () => "hello";
+//console.log(sayHello());
+const addNumbers =  (val1,val2)=>{
+	return val1+val2
+}
+console.log(typeof(5))
+module.exports = { sayHello,addNumbers };
---
+++ test/appTest.js
@@ -0,0 +1,24 @@
+const assert = require("chai").assert;
+const sayHello = require("../app").sayHello;
+const addNumbers = require('../app').addNumbers;
+
+describe("App", () => {
+  it("sayHello should return hello", function () {
+    let result = sayHello();
+    assert.equal(result, "hello");
+  });
+	it('sayHello should return type string',function(){
+		let result = sayHello();
+		assert.typeOf(result,"string")
+	});
+
+	it('add Numbers should be positive',function(){
+		let result = addNumbers(5,1);
+		assert.isAbove(result,0)
+	})
+
+	it('addNumbers should return type number',function(){
+		let result = addNumbers();
+		assert.typeOf(result,"number")
+	});
+});
